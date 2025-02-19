006b00a863d399e8fec2a09ecd8f084cfa3af177
Grigoriy Erdyakov <foyreet@yandex.ru>
2025-02-19 20:01:16 +0300

    Удалил старые папки

--- cpp_Lafore/2/exercise/5.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cctype> // для islower
-
-int main() {
-    char a;
-    std::cin >> a;
-    std::cout << islower(a) << std::endl;
-    return 0;
-}
--- cpp_Lafore/4/questions/17.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    enum speeds {obsolete = 78, single = 45, album = 33 };
-    std::cout << obsolete << std::endl;
-    std::cout << single << std::endl;
-    std::cout << album << std::endl;
-    return 0;
-}
