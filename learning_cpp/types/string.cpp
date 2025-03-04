e494fceeceb8cd9e3e6efbf7168fc64b3223d6b9
Grigoriy Erdyakov <foyreet@yandex.ru>
2025-03-04 16:18:29 +0300

    Проверка работы метода at в std::string

---
+++ learning_cpp/types/string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string s = "abc";
+
+    try {
+	std::cout << s.at(4) << std::endl;
+    } catch(const std::out_of_range& e) {
+	std::cout << "Error " << e.what() << std::endl;
+    }
+}
+
