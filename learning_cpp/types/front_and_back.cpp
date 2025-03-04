6cafafc509ae203f85fb291c7050c1b0b84ae573
Grigoriy Erdyakov <foyreet@yandex.ru>
2025-03-04 16:45:55 +0300

    Проверка работы методов front() and back()

---
+++ learning_cpp/types/front_and_back.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string s = "adasd";
+    std::cout << s << std::endl;
+    std::cout << "Первый символ: " << s.front() << std::endl;
+    std::cout << "Последний символ: " << s.back() << std::endl;
+}
